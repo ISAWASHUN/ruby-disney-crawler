44740b85b346524815a1dc933a93ddda822e7942
ISAWASHUN <syun20000616@icloud.com>
2024-01-13 11:21:04 +0900

    取得したい情報をDBに格納

--- app/models/hotel_plan.rb
+++ app/models/hotel_plan.rb
@@ -2,4 +2,28 @@ class HotelPlan < ApplicationRecord
   validates :plan_name, presence: true
   validates :room_name, presence: true, uniqueness: { scope: :plan_name }
   validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+
+  def self.fetch_hotel_plan
+    url = "https://asp.hotel-story.ne.jp/ver3d/planlist.asp?hcod1=00020&hcod2=001&hidmode=select&mode=seek&hidSELECTARRYMD=2019%2F08%2F12&hidSELECTHAKSU=2&hidSELECTadult=4&room=1&obj_ga=2.146060855.2096339239.1564882699-1444074251.1558508852&_ga=2.208041233.2096339239.1564882699-1444074251.1558508852"
+
+    agent = Mechanize.new
+    page = agent.get(url)
+
+    #旅行プランのタイトルを取得
+    page.search('.plan-list h3').each do |el|
+      plan_name = el.at('h3').text
+      table = el.at('.reserve_available')
+      table.search('tr').each_with_index do |row, i|
+        next if i == 0
+        room_name = row.at('.col01').text.squish
+        price =  row.at('.col3').text.squish
+
+        record = self.find_or_initialize_by(plan_name: plan_name, room_name: room_name)
+        record.price = price.gsub(/[^\d]/, '').to_i
+        record.save!
+      end
+      puts "=============="
+    end
+  end
 end
